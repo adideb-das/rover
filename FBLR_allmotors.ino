e71f2301e8addd0684f2f1a79aaa713ae423dd1a
Adideb Das <75713088+adideb-das@users.noreply.github.com>
2025-09-15 15:03:47 +0530

    Create FBLR_allmotors.ino

---
+++ FBLR_allmotors.ino
@@ -0,0 +1,92 @@
+// Motor A connections
+int M1A1 = 2;      // Motor A input 1
+int M1A2 = 3;      // Motor A input 2
+int EN_A = 9;      // Enable A (PWM)
+
+// Motor B connections
+int M2B1 = 4;      // Motor B input 1
+int M2B2 = 5;      // Motor B input 2
+int EN_B = 10;     // Enable B (PWM)
+
+void setup() {
+  // Motor pin setup
+  pinMode(M1A1, OUTPUT);
+  pinMode(M1A2, OUTPUT);
+  pinMode(M2B1, OUTPUT);
+  pinMode(M2B2, OUTPUT);
+  pinMode(EN_A, OUTPUT);
+  pinMode(EN_B, OUTPUT);
+
+  // Start Serial Monitor
+  Serial.begin(9600);
+  Serial.println("Motor driver ready...");
+
+  stopMotors();
+}
+
+void loop() {
+  forward();
+  delay(2000);
+
+  backward();
+  delay(2000);
+
+  left();
+  delay(2000);
+
+  right();
+  delay(2000);
+
+  stopMotors();
+  delay(2000);
+}
+
+// ---- Functions ----
+
+void backward() {
+  Serial.println("Moving Backward");
+  digitalWrite(M1A1, HIGH);
+  digitalWrite(M1A2, LOW);
+  digitalWrite(M2B1, HIGH);
+  digitalWrite(M2B2, LOW);
+  analogWrite(EN_A, 200);  // Speed 0-255
+  analogWrite(EN_B, 200);
+}
+
+void forward() {
+  Serial.println("Moving Forward");
+  digitalWrite(M1A1, LOW);
+  digitalWrite(M1A2, HIGH);
+  digitalWrite(M2B1, LOW);
+  digitalWrite(M2B2, HIGH);
+  analogWrite(EN_A, 200);
+  analogWrite(EN_B, 200);
+}
+
+void right() {
+  Serial.println("Turning Right");
+  digitalWrite(M1A1, LOW);
+  digitalWrite(M1A2, HIGH);
+  digitalWrite(M2B1, HIGH);
+  digitalWrite(M2B2, LOW);
+  analogWrite(EN_A, 200);
+  analogWrite(EN_B, 200);
+}
+
+void left () {
+  Serial.println("Turning Left");
+  digitalWrite(M1A1, HIGH);
+  digitalWrite(M1A2, LOW);
+  digitalWrite(M2B1, LOW);
+  digitalWrite(M2B2, HIGH);
+  analogWrite(EN_A, 200);
+  analogWrite(EN_B, 200);
+}
+
+void stopMotors() {
+  Serial.println("Stopping Motors");
+  digitalWrite(M1A1, LOW);
+  digitalWrite(M1A2, LOW);
+  digitalWrite(M2B1, LOW);
+  digitalWrite(M2B2, LOW);
+}
